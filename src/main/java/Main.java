3dda21b81ba772693e50d314e988844a63371728
Igor Pereslavtsev <Ig0r33@mail.ru>
2019-10-28 17:17:53 +0300

    add constants in Main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -8,26 +8,29 @@ import java.util.Random;
  * Class for showing how PersonArray work
  */
 public class Main {
+    private static int N = 20;
+    private static int K =5;
+    private static int MIN_YEAR = 1980;
+    private static int MAX_YEAR = 2018;
+    private static int MIN_PASSPORT_ID = 1000;
+    private static int MAX_PASSPORT_ID = 2000;
+    private static int MAX_MONTH = 12;
+    private static int MAX_DAY = 28;
+
     public static void main(String[] args) {
-        int n = 20;
-        int k=5;
-        int minYear = 1980;
-        int maxYear = 2018;
-        int minPassID = 1000;
-        int maxPassID = 2000;
-        int maxMonth = 12;
-        int maxDay = 28;
         Random random = new Random();
         PersonArray array = new PersonArray();
-        for (int i = 0; i < n; i++) {
-            array.add(new Person(new LocalDate(random.nextInt((maxYear - minYear) + 1) + minYear, random.nextInt(maxMonth) + 1, random.nextInt(maxDay)),
-                    random.nextInt(maxPassID - minPassID) + minPassID, Person.Gender.MAN,
+        for (int i = 0; i < N; i++) {
+            array.add(new Person(new LocalDate(random.nextInt((MAX_YEAR - MIN_YEAR) + 1) + MIN_YEAR,
+                    random.nextInt(MAX_MONTH) + 1,
+                    random.nextInt(MAX_DAY)),
+                    random.nextInt(MAX_PASSPORT_ID - MIN_PASSPORT_ID) + MIN_PASSPORT_ID, Person.Gender.MAN,
                     "PersonName" + i,
                     "PersonSurname" + i,
                     "PersonMiddleName" + i));
         }
         System.out.println(array);
-            array.delete(k, n / 2);
+            array.delete(K, N / 2);
         System.out.println(array);
         array.cutSize();
         System.out.println(array);
